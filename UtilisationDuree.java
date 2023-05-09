ed7cc930c60129e02cb62d2a7b190ca3f7a07cc3
Serres Fabien <fabien.serres@iut-rodez.fr>
2023-05-09 14:34:00 +0200

    Code initial

---
+++ UtilisationDuree.java
@@ -0,0 +1,20 @@
+/*
+ * UtilisationDuree.java
+ */
+
+package lesdurees;
+
+/**
+ * Utilisation des méthodes traitant les durées
+ * @author fabien.serres
+ */
+public class UtilisationDuree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         DureeHMS.testChaine();
+    }
+
+}

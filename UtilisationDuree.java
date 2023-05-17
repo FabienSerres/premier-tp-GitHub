0cd4b37d878196028e8c361ce937c324855bb2d0
Serres Fabien <fabien.serres@iut-rodez.fr>
2023-05-17 14:56:05 +0200

    Test commande pull request

--- UtilisationDuree.java
+++ UtilisationDuree.java
@@ -14,6 +14,7 @@ import lesdurees.DureeHMS;
 public class UtilisationDuree {
 
     /**
+     * Lancement des méthodes de test
      * @param args the command line arguments
      */
     public static void main(String[] args) {
